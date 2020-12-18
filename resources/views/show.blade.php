67bb1375ab7e525ab1c5f3890aa6f5e5a89042ee
igomis <igomis@cipfpbatoi.es>
2020-12-18 11:02:28 +0100

    Routes i vistes

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,24 @@
 @extends('layouts.master')
 @section('content')
-    Mostrar pel.licula id {{$pelicula}}
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="{{$pelicula->poster}}" alt=""/>
+        </div>
+        <div class="col-sm-8">
+            <h2>{{$pelicula->title}}</h2>
+            <h4>{{$pelicula->director}}</h4>
+            <h4>{{$pelicula->year}}</h4>
+            <p><strong>Resumen: {{$pelicula->synopsis}}</strong></p>
+            <p><strong>Estado: </strong>Pel.lícula @if ($pelicula->rented)  Actualment llogada @else disponible @endif</p>
+            <p>
+                @if ($pelicula->rented)
+                    <a href='' class="btn btn-info">Tornar Pel.lícula</a>
+                @else
+                    <a href='' class="btn btn-danger">Llogar Pel.lícula</a>
+                @endif
+                <a href="{{ route('movie.edit',$pelicula->id) }}" class="btn btn-warning"><i class="fa fa-pencil"></i>Editar Pel.lícula</a>
+                <a href="{{ route('movie.index') }}" class="btn btn-default">Tornar catàleg</a>
+            </p>
+        </div>
+    </div>
 @stop
